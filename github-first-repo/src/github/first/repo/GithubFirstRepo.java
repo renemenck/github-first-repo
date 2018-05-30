f6b4e4233e05efeed8c509ac9a5fca3cd812b08d
Rene <Rene@DESKTOP-LPI2L35>
2018-05-30 08:39:26 -0300

    Imp0rementado funções na rotina main

--- github-first-repo/src/github/first/repo/GithubFirstRepo.java
+++ github-first-repo/src/github/first/repo/GithubFirstRepo.java
@@ -16,6 +16,8 @@ public class GithubFirstRepo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Olá Mundo");
+                
     }
     
 }
